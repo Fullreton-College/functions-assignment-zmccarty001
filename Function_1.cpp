3feceff81694ccdca2e2654623951ee73c20a269
Turner, Zachariah <z00114761@gmail.com>
2026-03-05 15:59:07 -0800

    changes made

--- Function_1.cpp
+++ Function_1.cpp
@@ -3,13 +3,32 @@
 using namespace std;
 
 /* Define your function here */ 
+string coinFlip() {
+   int r =rand() % 2;
+   if (r == 1) 
+      return "Heads.";
+   else
+      return "Tails.";
+   
+}
 
 int main() {
    // Add more variables as needed
+   int numFlips;
+   
+
+
    
    srand(2);  // Unique seed
 
    /* Type your code here */
+   cout<<"How many times would you like to flip the coin?\n";
+   cin>>numFlips;
+   for (int i= 0; i<numFlips; i++) {
+      cout<<coinFlip();
+      cout<<endl;
+   }
+
 
    return 0;
 }
